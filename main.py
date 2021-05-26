6fa7b37b4e956a533ced6b3157e32041de5f57f5
gyupro <gyupro89@gmail.com>
2021-05-26 11:31:31 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
