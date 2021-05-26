65e4c42655d2b39e71e033846cb4563808cc1194
gyupro <gyupro89@gmail.com>
2021-05-26 11:37:02 +0900

    test commit

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import numpy as np
 import streamlit as st
 
 
+test = input()
+
+
+for i in range(int(15)):
+    print(i)
+
+
 dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
+    np.random.randn(range(15)),
     columns=('col %d' % i for i in range(20)))
 st.table(dataframe)
